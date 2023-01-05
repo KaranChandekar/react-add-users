0adad51cd03e98c1ed52675aabe35a40ecb96411
Karan <karanchandekar431@gmail.com>
2023-01-05 11:16:17 +0530

    added a user component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
+import React from "react";
+import AddUser from "./Components/Users/AddUser";
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }

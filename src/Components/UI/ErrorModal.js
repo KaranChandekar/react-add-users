9fb8317e6a6e620a4d54ad33bd52569baef5fab5
Karan <karanchandekar431@gmail.com>
2023-01-06 20:27:58 +0530

    added a react fragment

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -6,7 +6,7 @@ import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <div>
+    <React.Fragment>
       <div className={classes.backdrop} onClick={props.onConfirm}></div>
       <Card className={classes.modal}>
         <header className={classes.header}>
@@ -19,7 +19,7 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Ok</Button>
         </footer>
       </Card>
-    </div>
+    </React.Fragment>
   );
 };
 
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -44,7 +44,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <React.Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -71,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </React.Fragment>
   );
 };
 
